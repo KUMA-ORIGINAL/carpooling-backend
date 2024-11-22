2e21a6e74107aa63783f82f153fe5def1d25516c
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2024-11-22 21:26:48 +0600

    first commit

---
+++ app/accounts/serializers.py
@@ -0,0 +1,17 @@
+from djoser.serializers import UserCreateSerializer, UserSerializer
+
+from .models import User
+
+
+class UserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ('id', 'email', 'username', 'first_name', 'last_name',
+                  'password')
+
+
+class UserSerializer(UserSerializer):
+
+    class Meta(UserSerializer.Meta):
+        fields = ('id', 'email', 'username', 'first_name', 'last_name',
+                  'role')
